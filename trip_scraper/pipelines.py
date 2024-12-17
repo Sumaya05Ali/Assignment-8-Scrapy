2a593586130d434a99e755e0179021d808ddd2a1
Sumaya05Ali <123470887+Sumaya05Ali@users.noreply.github.com>
2024-12-17 17:48:47 +0600

    Update pipelines.py

--- trip_scraper/pipelines.py
+++ trip_scraper/pipelines.py
@@ -1,13 +1,38 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from .models import Hotel, City, init_db
 
+import os
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+class PostgresPipeline:
+    def __init__(self):
+        self.session = init_db()
 
-
-class TripScraperPipeline:
     def process_item(self, item, spider):
+        # Process the hotel data
+        hotel = Hotel(
+            title=item['title'],
+            rating=item['rating'],
+            location=item['location'],
+            latitude=item['latitude'],
+            longitude=item['longitude'],
+            room_type=item['room_type'],
+            price=item['price'],
+            image=item['image']  # Save image reference path
+        )
+
+        # Check if the city exists, if not, create a new city record
+        city_name = item['city']
+        city = self.session.query(City).filter_by(name=city_name).first()
+        if not city:
+            city = City(name=city_name)
+            self.session.add(city)
+            self.session.commit()
+
+        # Assign the city to the hotel
+        hotel.city_id = city.id
+        self.session.add(hotel)
+        self.session.commit()
+
         return item
+
+    def close_spider(self, spider):
+        self.session.close()
