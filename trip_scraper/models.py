6d4c144b0d3c162a058de488df204219dd361387
Sumaya05Ali <123470887+Sumaya05Ali@users.noreply.github.com>
2024-12-17 17:49:30 +0600

    Update models.py

--- trip_scraper/models.py
+++ trip_scraper/models.py
@@ -3,23 +3,28 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
-
+def init_db():
+    DATABASE_URL = "postgresql://user:password@db:5432/trip_scraper_db"
+    engine = create_engine(DATABASE_URL)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    return Session()
 # Hotel model
 class Hotel(Base):
     __tablename__ = 'hotels'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    title = Column(String)
     rating = Column(String)
     location = Column(String)
     latitude = Column(Float)
     longitude = Column(Float)
-    price = Column(Float)
     room_type = Column(String)
+    price = Column(Float)
+    image = Column(String)  # Image path reference
 
     city_id = Column(Integer, ForeignKey('cities.id'))
     city = relationship('City', back_populates='hotels')
-    images = relationship('Image', back_populates='hotel')
 
 # City model
 class City(Base):
@@ -29,20 +34,4 @@ class City(Base):
     name = Column(String)
     hotels = relationship('Hotel', back_populates='city')
 
-# Image model
-class Image(Base):
-    __tablename__ = 'images'
 
-    id = Column(Integer, primary_key=True)
-    file_path = Column(String)
-    hotel_id = Column(Integer, ForeignKey('hotels.id'))
-    hotel = relationship('Hotel', back_populates='images')
-
-def init_db():
-    # Initialize the database connection
-    DATABASE_URL = "postgresql://user:password@db:5432/trip_scraper_db"
-    engine = create_engine(DATABASE_URL)
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    return Session()
